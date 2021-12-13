e29b03dcd9dc7583fcdad4c15acc09055c28e1cf
AndreDantasss <andre.viictor@gmail.com>
2021-12-13 04:19:56 -0300

    SistemaAluno

---
+++ sistema.js
@@ -0,0 +1,40 @@
+// Aula remota (09/12/21)
+
+let nomeDeAluno = ["André", "Victor", "Maria", "Raquel"]
+
+for (let nome of nomeDeAluno){
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+
+
+// let numerodealunos = 10;
+
+// let contador = 0;
+
+// while (contador <= numerodealunos) {
+//     //console.log(contador)  
+
+// if (contador == 0){
+//     console.log("O número atual é zero!")
+// } else if (contador % 2 == 0){
+//     console.log(`O número ${contador} é par!`)
+// } else {
+//     console.log(`O número ${contador} é ímpar!`)
+// }
+// contador++;
+// }
+
+
+
+// for (let contador = 0; contador <= numerodealunos; contador++) {
+//     //console.log(contador)  
+
+// if (contador == 0){
+//     console.log("O número atual é zero!")
+// } else if (contador % 2 == 0){
+//     console.log(`O número ${contador} é par!`)
+// } else {
+//     console.log(`O número ${contador} é ímpar!`)
+// }
+// }
